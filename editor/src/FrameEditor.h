4399a572940deae2e2e5101b59c6e75e4f60c48a
fdelapena <fdelapena@c7c03aae-3e16-0410-b5dd-fde515843ac6>
2010-12-06 07:36:03 +0000

    Updated homepage URI
    git-svn-id: https://easyrpg.svn.sourceforge.net/svnroot/easyrpg/trunk@870 c7c03aae-3e16-0410-b5dd-fde515843ac6

--- editor/src/FrameEditor.h
+++ editor/src/FrameEditor.h
@@ -1,5 +1,5 @@
 /* This file is part of EasyRPG editor.
-Copyright (C) 2007-2009 EasyRPG Project <http://easyrpg.sourceforge.net/>.
+Copyright (C) 2007-2009 EasyRPG Project
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
