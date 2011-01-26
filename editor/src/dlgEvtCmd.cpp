f595c6eeb2978b50bc21379d0696dbb3da639fae
fdelapena <fdelapena@c7c03aae-3e16-0410-b5dd-fde515843ac6>
2011-01-26 08:57:21 +0000

    Add methods for EvtCmd buttons.
    git-svn-id: https://easyrpg.svn.sourceforge.net/svnroot/easyrpg/trunk@1207 c7c03aae-3e16-0410-b5dd-fde515843ac6

--- editor/src/dlgEvtCmd.cpp
+++ editor/src/dlgEvtCmd.cpp
@@ -16,8 +16,131 @@
 /////////////////////////////////////////////////////////////////////////////
 
 #include "dlgEvtCmd.h"
+#include "dlgEvtCmdMessage.h"
 
 dlgEvtCmd::dlgEvtCmd(wxWindow* parent) : dlgEvtCmd_Base(parent) {
+}
+
+void dlgEvtCmd::btnMessage_click(wxCommandEvent& WXUNUSED(event)) {
+	dlgEvtCmdMessage *DialogEvtCmdMessage = new dlgEvtCmdMessage(NULL);
+	DialogEvtCmdMessage->ShowModal();
+	DialogEvtCmdMessage->Destroy();
+}
+
+void dlgEvtCmd::btnMessageOptions_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnFaceGraphic_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnChoice_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnNumberInput_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnSwitch_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnVariable_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnTimer_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnMoney_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnItem_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnParty_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnExperience_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnLevel_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnParameter_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnSkill_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnEquipment_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnHP_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnMP_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnState_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnRecover_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnDamage_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnActorName_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnActorTitle_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnActorGraphic_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnActorFaceGraphic_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnVehicleGraphic_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnSystemMusic_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnSystemSound_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnSystemGraphic_click(wxCommandEvent& WXUNUSED(event)) {
+	
+}
+
+void dlgEvtCmd::btnTransition_click(wxCommandEvent& WXUNUSED(event)) {
+	
 }
 
 void dlgEvtCmd::Close_click(wxCommandEvent& WXUNUSED(event)) {
