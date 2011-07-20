8513a1c63e9f2b12bb0dfaea444a920d5fba981d
takecheeze <takecheeze@c7c03aae-3e16-0410-b5dd-fde515843ac6>
2011-07-20 13:52:39 +0000

    create some common cmake module
    git-svn-id: https://easyrpg.svn.sourceforge.net/svnroot/easyrpg/trunk@1482 c7c03aae-3e16-0410-b5dd-fde515843ac6

---
+++ cmake_module/FindPixman.cmake
@@ -0,0 +1,9 @@
+find_path(PIXMAN_INCLUDE_DIR_INTERNAL pixman-1/pixman.h)
+find_library(PIXMAN_LIBRARY pixman-1)
+if((EXISTS ${PIXMAN_INCLUDE_DIR_INTERNAL}) AND (EXISTS ${PIXMAN_LIBRARY}))
+  set(Pixman_FOUND TRUE)
+  set(PIXMAN_INCLUDE_DIR "${PIXMAN_INCLUDE_DIR_INTERNAL}/pixman-1")
+else()
+  set(Pixman_FOUND FALSE)
+endif()
+
