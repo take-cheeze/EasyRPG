abe497bc7bc12b3cde8088ad656f98f50155f1c2
Takeshi Watanabe <takechi101010@gmail.com>
2011-10-27 04:40:53 +0000

    fix to FindIconv.cmake
    git-svn-id: https://easyrpg.svn.sourceforge.net/svnroot/easyrpg/trunk@1515 c7c03aae-3e16-0410-b5dd-fde515843ac6

--- cmake_module/FindIconv.cmake
+++ cmake_module/FindIconv.cmake
@@ -1,7 +1,11 @@
 find_path(ICONV_INCLUDE_DIR iconv.h)
 find_library(ICONV_LIBRARY iconv)
-if((EXISTS ${ICONV_INCLUDE_DIR}) AND (EXISTS ${ICONV_LIBRARY}))
+if(EXISTS ${ICONV_INCLUDE_DIR})
   set(Iconv_FOUND TRUE)
+
+  if(NOT EXISTS ${ICONV_LIBRARY})
+    set(ICONV_LIBRARY "")
+  endif()
   if((${APPLE}) AND (${ICONV_LIBRARY} MATCHES "/usr/lib"))
     string(REPLACE "/usr/lib" "/opt/local/lib" macports_iconv_library ${ICONV_LIBRARY})
     if(EXISTS ${macports_iconv_library})
